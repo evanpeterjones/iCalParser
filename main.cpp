6b32a2e8dd70ef46abc53d0f9fa78df8609cea9d
evanpeterjones <evanpeterjones@gmail.com>
2018-10-11 00:50:38 -0400

    started work pulling ical file using cURLpp, connects, but needs to store file

--- main.cpp
+++ main.cpp
@@ -14,6 +14,7 @@
 #include <unistd.h>
 #include <fstream>
 #include <time.h>
+#include <curl/curl.h>
 
 using namespace std;
 
@@ -37,7 +38,8 @@ void printEvent(int year, int month, int day, string name, string time) {
   printf("%s %d, %d:%.15s%s\n", str_month.c_str(), day, year, name.c_str(), time.c_str());
 }
 
-std::string getDate() {
+std::string getDate()
+{
   time_t tim = time(NULL);
   struct tm *atime = localtime(&tim);
   int year, month, day;
@@ -45,15 +47,47 @@ std::string getDate() {
   c_day = atime->tm_mday;
   c_month = atime->tm_mon + 1;
   c_year = atime->tm_year+1900;
-  string currentDate = to_string(c_month)+"/"+to_string(c_day)+"/"+to_string(c_year);
+  return to_string(c_month)+"/"+to_string(c_day)+"/"+to_string(c_year);
 }
 
-int main(int argc, const char * argv[]) {
-  string url = "https://calendar.google.com/calendar/ical/evan.peter.jones%40gmail.com/public/basic.ics";
+FILE * getFile(string url)
+{
+  CURL *curl = curl_easy_init();
+  FILE *ical;
+  CURLcode res;
+  if (curl)
+  {
+    ical = fopen("cal.ical","wb");
+    curl_easy_setopt(curl, CURLOPT_URL, url);
+    curl_easy_setopt(curl, CURLOPT_ACCEPT_ENCODING, "deflate");
+    //    curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_data);
+    curl_easy_setopt(curl, CURLOPT_WRITEDATA, ical);
+    res = curl_easy_perform(curl);
+    curl_easy_cleanup(curl);
+    if ( res != CURLE_OK ) 
+    {
+      cout << curl_easy_strerror(res) << endl;
+    }
+    return ical;
+  }  
+}
+
+int main(int argc, const char * argv[])
+{
+
+  string url = "https://calendar.google.com/calendar/ical/evan.peter.jones%40gmail.com/private-2f9edde9e0ab999456847eaf507d680d/basic.ics";
+#ifdef SKIP_PEER_VERIFICATION
+  curl_easy_setopt(curl, CURLOPT_SSL_VERIFYPEER, 0L);
+#endif
+#ifdef SKIP_HOSTNAME_VERIFICATION
+  curl_easy_setopt(curl, CURLOPT_SSL_VERIFYHOST, 0L);
+#endif  
+  FILE *ical = getFile(url);
   string Date = getDate();  
   std::cout << Date << std::endl;
+  std::cout << ical << std::endl;
 
-  /*
+
   bool v;
   string data;
   string summ;
@@ -81,7 +115,7 @@ sadflkj	}
       } while (data.find(SUMMARY) == std::string::npos);//this means while the value doesn't match we run;
     }
   }
-  f.close();*/
+  f.close();
   return 0;
 }
 
