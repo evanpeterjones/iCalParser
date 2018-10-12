ffb3f6dbb078e5e8ca8729552fc9c20cd411bf3c
evanpeterjones <evanpeterjones@gmail.com>
2018-10-12 19:27:49 -0400

    removed namespace

--- main.cpp
+++ main.cpp
@@ -16,26 +16,23 @@
 #include <time.h>
 #include <curl/curl.h>
 
-using namespace std;
-
-void printEvent(int year, int month, int day, string name, string time) {
-  string str_month;
+std::string getMonth(int month)
+{
   switch (month) {
-  case 1  : str_month = "January"; break;
-  case 2  : str_month = "February";break;
-  case 3  : str_month = "March";break;
-  case 4  : str_month = "April";break;
-  case 5  : str_month = "May";break;
-  case 6  : str_month = "June";break;
-  case 7  : str_month = "July";break;
-  case 8  : str_month = "August";break;
-  case 9  : str_month = "September";break;
-  case 10 : str_month = "October";break;
-  case 11 : str_month = "November";break;
-  case 12 : str_month = "December";break;
-  default : str_month = "FUCK";break;
+  case 1  : return "January";
+  case 2  : return "February";
+  case 3  : return "March";
+  case 4  : return "April";
+  case 5  : return "May";
+  case 6  : return "June";
+  case 7  : return "July";
+  case 8  : return "August";
+  case 9  : return "September";
+  case 10 : return "October";
+  case 11 : return "November";
+  case 12 : return "December";
+  default : return "FUCK";
   }
-  printf("%s %d, %d:%.15s%s\n", str_month.c_str(), day, year, name.c_str(), time.c_str());
 }
 
 std::string getDate()
@@ -47,10 +44,10 @@ std::string getDate()
   c_day = atime->tm_mday;
   c_month = atime->tm_mon + 1;
   c_year = atime->tm_year+1900;
-  return to_string(c_month)+"/"+to_string(c_day)+"/"+to_string(c_year);
+  return std::to_string(c_month)+"/"+std::to_string(c_day)+"/"+std::to_string(c_year);
 }
 
-FILE * getFile(string url)
+FILE * getFile(std::string url)
 {
   CURL *curl = curl_easy_init();
   FILE *ical;
@@ -66,7 +63,7 @@ FILE * getFile(string url)
     curl_easy_cleanup(curl);
     if ( res != CURLE_OK ) 
     {
-      cout << curl_easy_strerror(res) << endl;
+      std::cout << curl_easy_strerror(res) << std::endl;
     }
     return ical;
   }  
@@ -74,21 +71,14 @@ FILE * getFile(string url)
 
 int main(int argc, const char * argv[])
 {
-
-  string url = "https://calendar.google.com/calendar/ical/evan.peter.jones%40gmail.com/private-2f9edde9e0ab999456847eaf507d680d/basic.ics";
-#ifdef SKIP_PEER_VERIFICATION
-  curl_easy_setopt(curl, CURLOPT_SSL_VERIFYPEER, 0L);
-#endif
-#ifdef SKIP_HOSTNAME_VERIFICATION
-  curl_easy_setopt(curl, CURLOPT_SSL_VERIFYHOST, 0L);
-#endif  
+  std::string url = "https://calendar.google.com/calendar/ical/evan.peter.jones%40gmail.com/private-2f9edde9e0ab999456847eaf507d680d/basic.ics";
   FILE *ical = getFile(url);
-  string Date = getDate();  
+  std::string Date = getDate();
   std::cout << Date << std::endl;
   std::cout << ical << std::endl;
 
 
-  bool v;
+  /*bool v;
   string data;
   string summ;
   fstream f;
@@ -115,7 +105,7 @@ sadflkj	}
       } while (data.find(SUMMARY) == std::string::npos);//this means while the value doesn't match we run;
     }
   }
-  f.close();
+  f.close();*/
   return 0;
 }
 
