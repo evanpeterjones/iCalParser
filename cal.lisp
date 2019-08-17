5103c4d65a39293d37bdb2f1f5dd3e440824ebc7
evanpeterjones <evanpeterjones@gmail.com>
2019-08-17 17:34:14 -0400

    fixed my file-slurp method and etc

--- cal.lisp
+++ cal.lisp
@@ -1,26 +1,27 @@
 ;; 
-;; load google calendar
+;; load and parse google calendar -- currently only downloads my google calendar
 ;;
 ;; author: evan jones
 ;;
 
 (ql:quickload '(:drakma :cl-ppcre) :silent t)
 
+;; (load "file-type.lisp") ehhhh
+
 (defpackage calendar
-  (:use :common-lisp :cl-ppcre))
+  (:use :common-lisp :cl-ppcre :file-type-utility))
 
 (in-package calendar)
 
 (defparameter *url* "https://calendar.google.com/calendar/ical/evan.peter.jones%40gmail.com/private-2f9edde9e0ab999456847eaf507d680d/basic.ics")
-(defparameter *ical-properties* '("EVENT" "DTSTART" "DTEND" "RRULE"))
+(defparameter *ical-properties* '("EVENT" "DTSTART" "DTEND" "RRULE") "these are properties described by the ICS international standard")
 
-(defun file-to-string (stream)
-  "read a file stream into a string (currently returns a cons list, yikes)"
-  (if stream
-      (loop for line = (read-line stream nil)
-           while line
-           collect line)
-      (format t "stream init is nil")))
+(defun file-to-string (file-name)
+  "read a file stream into a string"
+  (with-open-file (stream file-name :if-does-not-exist nil)
+    (let ((data (make-string (file-length stream))))
+      (read-sequence data stream)
+      data)))
 
 (defun save-to-ical-file (file file-name)
   "read the result of an HTTP request to disk"
@@ -32,17 +33,15 @@
         (format str file))
       (format t "http request returned nil")))
 
-(defun get-ical-file (url)
+(defun read-ical-file (url)
+  "handle requesting ical file from a url"
   (let ((file-name "cal.ics"))
-    "handle a request to a url"
-    (if (probe-file file-name)
-        (file-to-string (open file-name))
-        (progn
-          (save-to-ical-file (drakma:http-request url) file-name)
-          (get-ical-file file-name)))))
+    (if (not (probe-file file-name))
+        (save-to-ical-file (drakma:http-request url) file-name))
+    (file-to-string file-name)))
 
 ;; todo : make sure get-ical-file returns as string instead of cons list
 ;;; then split string by VEVENTS, then maybe learn CLOS so we can create dynamic
 ;;; objects using the string data. 
 
-(defvar ical-file (get-ical-file *url*))
+(defvar f-ical (read-ical-file *url*))
